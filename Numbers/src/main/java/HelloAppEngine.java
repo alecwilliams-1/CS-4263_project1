cb57945a80b9dab5e4bf48ca4211d7fe7573f1a1
Riley Pate <riley1234@sbcglobal.net>
2019-09-25 17:19:09 -0500

    Added attempted App Engine Java implementation
    Inside folder called Numbers

---
+++ Numbers/src/main/java/HelloAppEngine.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.util.Random;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+    name = "RandomNumbers",
+    urlPatterns = {"/hello"}
+)
+public class HelloAppEngine extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException {
+
+    response.setContentType("text/plain");
+    response.setCharacterEncoding("UTF-8");
+
+    Random random = new Random();
+    int number = random.nextInt(999999)+1;
+    System.out.println(number);
+  }
+}

f7bad73f0ca1ea8047bda769fbb27b5dd3f5506a
RileyPate <riley1234@sbcglobal.net>
2020-03-10 14:52:46 -0500

    Created DataFrame and filtered outliers

--- Python/main.py
+++ Python/main.py
@@ -1,5 +1,6 @@
 import pyodbc
 import pandas as pd
+from pandas import DataFrame
 server = 'tcp:sp2020.database.windows.net'
 database = 'SP2020'
 username = 'se2sql'
@@ -8,10 +9,11 @@ password = 'SoftwareEngineering2'
 #Gets full rows of candidates
 cnxn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
-cursor.execute("SELECT * FROM dbo.cands")
+sql = "SELECT * FROM dbo.cands"
+# cursor.execute(sql)
 
 # cursor.execute("SELECT Party, count(Party) AS CountOf FROM dbo.cands GROUP BY Party HAVING count(Party) >3")
-
+'''
 #Testing connecting twice to same server
 cnxn2 = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor2 = cnxn2.cursor()
@@ -24,3 +26,8 @@ for row in cursor:
 
 for row2 in cursor2:
     print(row2)
+'''
+df = pd.read_sql(sql, cnxn)
+# Parse away outliers to get only five choice of party affiliation
+df = df[(df.Party == 'R') | (df.Party == 'D')| (df.Party == '3') | (df.Party == 'L') | (df.Party == 'U')]
+print(df)
